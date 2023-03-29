80984eeabe3f69efca491b0266b24c2609bba0c0
Sudhakarsiva18 <108916787+Sudhakarsiva18@users.noreply.github.com>
2023-03-29 10:50:38 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+import Location from './Components/Challenge Yourself/Location'
+import Car from './Components/Challenge Yourself/Car'
+
+function App() {
+  return (
+    <div>
+      <center>
+      <Car/>
+      <Location/>
+      </center>
+    </div>
+  )
+}
+
+export default App
---
+++ Location.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Location() {
+  return (
+    <div>
+        <p>Company Name:AUDI</p>
+        <h>City Name:Chennai</h>
+        </div>
+    
+  );
+}
+
+export default Location
